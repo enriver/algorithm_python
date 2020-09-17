7c3c81b6f46aa0f9f40fa00396212bb478ff2fb7
enriver <riverkeh@naver.com>
2020-09-17 19:41:07 +0900

    0917 커밋

---
+++ Class_1/q3053.py
@@ -0,0 +1,9 @@
+#택시 기하학
+
+import sys
+import math
+
+n=int(sys.stdin.readline())
+
+print(round(math.pi*n**2,6))
+print(round(2*n**2,6))
--- Class_2/q1926.py
+++ Class_2/q1926.py
@@ -1,48 +1,44 @@
 #그림
 
 import sys
-from collections import deque
+
 sys.setrecursionlimit(1000000)
+n,m = map(int,sys.stdin.readline().split()) # n행 m렬
 
-n,m=map(int,sys.stdin.readline().split())
 
-mat=[[0 for _ in range(m)] for _ in range(n)]
-visit=[[False for _ in range(m)] for _ in range(n)]
+visit=[[False for _  in range(501)] for _ in range(501)]
+mat=[[0 for _ in range(501)] for _ in range(501)]
 
 for i in range(n):
-    k=list(map(int,sys.stdin.readline().split()))
+    val=list(map(int , sys.stdin.readline().split()))
     for j in range(m):
-        mat[i][j]=k[j]
+        mat[i][j]=val[j]
 
-dx=[0,1,0,-1]
-dy=[1,0,-1,0]
+dx=[1,-1,0,0]
+dy=[0,0,1,-1]
 
-def dfs(x,y,depth):
+def dfs(cnt,x,y):
+
     for i in range(4):
         nx,ny=x+dx[i], y+dy[i]
         if 0<=nx<n and 0<=ny<m:
             if not visit[nx][ny] and mat[nx][ny]==1:
                 visit[nx][ny]=True
-                depth=dfs(nx,ny,depth+1)
-    return depth
-    
+                cnt=dfs(cnt+1,nx,ny)
 
-stack=[]
+    return cnt
 
-isZero=True
+
+picture_num=0
+picture_size=0
 
 for i in range(n):
     for j in range(m):
         if not visit[i][j] and mat[i][j]==1:
-            isZero=False
             visit[i][j]=True
-            stack.append(dfs(i,j,1))
+            picture_num+=1
+            picture_size=max(picture_size,dfs(1,i,j))
+           
+print(picture_num)
+print(picture_size)
 
-if isZero==True:
-    print(0)
-    print(0)
-else:
-    stack.sort()
-
-    print(len(stack))
-    print(stack[-1])
