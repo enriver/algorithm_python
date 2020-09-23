3caf48f800dd894863819d2c53beb76013e77d8c
enriver <riverkeh@naver.com>
2020-09-24 01:44:28 +0900

    0924 커밋

--- Class_3/q4811.py
+++ Class_3/q4811.py
@@ -3,17 +3,26 @@
 import sys
 
 
-dp=[[0 for _ in range(31)] for _ in range(31)]
-
-
 def pill(W,H):
 
-        
+    if dp[W][H]>0:
+        return dp[W][H]
+    
+    if W==0:
+        return 1
+    
+    if W>0 and H==0:
+        dp[W][H]+=pill(W-1,H+1)
+    elif W>0 and H>0:
+        dp[W][H]+=pill(W-1,H+1)
+        dp[W][H]+=pill(W,H-1)
 
-
+    return dp[W][H]
+    
 
 while True:
     n=int(sys.stdin.readline())
+    dp=[[0 for _ in range(31)] for _ in range(31)]
 
     if n==0:
         break
