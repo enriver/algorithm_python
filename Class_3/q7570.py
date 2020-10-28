f882a3055099cc23608eb10b2081dded6c5db83a
enriver <riverkeh@naver.com>
2020-10-29 00:40:31 +0900

    1029  커밋

--- Class_3/q7570.py
+++ Class_3/q7570.py
@@ -4,4 +4,11 @@ import sys
 
 n=int(sys.stdin.readline())
 
-arr=list(map(int,sys.stdin.readline().split())
+arr=list(map(int,sys.stdin.readline().split()))
+
+dp=[0]*(n+1)
+
+for i in range(n):
+    dp[arr[i]]=dp[arr[i]-1]+1
+
+print(n-max(dp))
