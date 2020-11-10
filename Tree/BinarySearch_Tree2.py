6bcc5a2d4498e5f00c8f624d15ae75f736b24944
enriver <riverkeh@naver.com>
2020-11-10 23:57:18 +0900

    이진트리 구현 연습

---
+++ Tree/BinarySearch_Tree2.py
@@ -0,0 +1,84 @@
+# 이진 검색 트리
+
+import sys
+
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.left=None
+        self.right=None
+
+
+class Tree:
+    def __init__(self,root):
+        self.root=root
+
+    def insert(self, data):
+        self.current_node=self.root
+
+        while True:
+            if data<self.current_node.data:
+                if self.current_node.left!=None:
+                    self.current_node=self.current_node.left
+                else:
+                    self.current_node.left=Node(data)
+                    break
+            else:
+                if self.current_node.right!=None:
+                    self.current_node=self.current_node.right
+                else:
+                    self.current_node.right=Node(data)
+                    break
+    
+    #전위
+    def preorder(self):
+        def _pre_order(root):
+            if root is None:
+                pass
+            else:
+                print(root.data,end='->')
+                _pre_order(root.left)
+                _pre_order(root.right)
+        _pre_order(self.root)
+
+    #중위
+    def inorder(self):
+        def _in_order(root):
+            if root is None:
+                pass
+            else:
+                _in_order(root.left)
+                print(root.data,end='->')
+                _in_order(root.right)
+        _in_order(self.root)
+
+    #후위
+    def postorder(self):
+        def _post_order(root):
+            if root is None:
+                pass
+            else:
+                _post_order(root.left)
+                _post_order(root.right)
+                print(root.data, end='->')
+        _post_order(self.root)
+
+
+if __name__ == "__main__":
+    root=Node(int(sys.stdin.readline()))
+    tree=Tree(root)
+
+    while True:
+        try:
+            tree.insert(int(sys.stdin.readline()))
+        except:
+            break
+    
+    print('전위 순회')
+    tree.preorder()
+    print()
+    print('후위 순회')
+    tree.postorder()
+    print()
+    print('중위 순회')
+    tree.inorder()
