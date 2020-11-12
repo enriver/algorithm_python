e55ec1bbad4dcc338fc626513dbe38e7483ca11e
enriver <riverkeh@naver.com>
2020-11-13 00:42:06 +0900

    ITDA 3rd - week7

---
+++ 그리디/q1946.py
@@ -0,0 +1,10 @@
+# 신입사원 - S1
+
+import sys
+
+T=int(sys.stdin.readline())
+
+for _ in range(T):
+    N=int(sys.stdin.readline())
+    r1,r2=map(int,sys.stdin.readline())
+    
---
+++ 이분탐색/q15810.py
@@ -0,0 +1,7 @@
+# 풍선 공장 - S2
+
+import sys
+
+N,M = map(int,sys.stdin.readline().split())
+
+time=list(map(int,sys.stdin.readline().split()))
