74a6a68fcab72b6941a05fa3b6a85d053167dd2e
enriver <riverkeh@naver.com>
2020-11-16 17:18:25 +0900

    ADD BOJ_1946

--- 그리디/q1946.py
+++ 그리디/q1946.py
@@ -6,5 +6,20 @@ T=int(sys.stdin.readline())
 
 for _ in range(T):
     N=int(sys.stdin.readline())
-    r1,r2=map(int,sys.stdin.readline())
+
+    employee=list()
+    for _ in range(N):
+        r1,r2=map(int,sys.stdin.readline().split())
+        employee.append((r1,r2))
     
+    employee.sort()
+
+    count=1
+    standard=employee[0][1]
+
+    for i in range(1,N):
+        if employee[i][1]<standard:
+            standard=employee[i][1]
+            count+=1
+
+    print(count)
