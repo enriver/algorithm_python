c055e6eff3cd018907e5800a5a58cd1278f35dce
enriver <riverkeh@naver.com>
2020-12-01 00:44:17 +0900

    UPDATE BOJ_9205

--- BFS DFS/q9205.py
+++ BFS DFS/q9205.py
@@ -3,6 +3,7 @@
 import sys
 from collections import deque
 
+#dfs 풀이
 
 def dfs(num):
     global result
@@ -33,3 +34,42 @@ for _ in range(t):
 
     print(result)
     
+'''
+# bsf 풀이
+
+def bfs(x,y):
+    global result
+    que=deque()
+    que.append((x,y))
+
+    while que:
+        x,y=que.popleft()
+
+        if (x,y)==position[-1]:
+            result='happy'
+            return
+        
+        for i, value in enumerate(position):
+            dx=abs(value[0]-x)
+            dy=abs(value[1]-y)
+
+            if dx+dy<=1000 and not visit[i]:
+                visit[i]=True
+                bfs(value[0],value[1])
+    return
+
+t=int(sys.stdin.readline())
+for _ in range(t):
+    n=int(sys.stdin.readline())
+    position=list()
+    visit=[False for _ in range(n+2)]
+
+    for _ in range(n+2):
+        x,y=(map(int,sys.stdin.readline().split()))
+        position.append((x,y))
+    
+    result='sad'
+    bfs(position[0][0],position[0][1])
+
+    print(result)
+'''
