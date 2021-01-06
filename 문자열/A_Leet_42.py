bae6c21492f2635e6e367ead74a8ba456d3f2f2c
enriver <riverkeh@naver.com>
2021-01-06 12:01:11 +0900

    LEET

---
+++ 문자열/A_Leet_42.py
@@ -0,0 +1,23 @@
+# Trapping Rain Water
+
+from typing import List
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        stack=list()
+        volume=0
+        
+        for i in range(len(height)):
+            while stack and height[i]>height[stack[-1]]:
+                top=stack.pop()
+                
+                if not len(stack):
+                    break
+                
+                distance=i-stack[-1]-1
+                waters=min(height[i], height[stack[-1]])-height[top]
+                
+                volume+=distance*waters
+                
+            stack.append(i)
+        return volume
---
+++ 문자열/Leet_561.py
@@ -0,0 +1,13 @@
+# Array Partition I.
+
+from typing import List
+
+class Solution:
+    def arrayPairSum(self, nums: List[int]) -> int:
+        nums.sort()
+        result=0
+        
+        for i in range(0,len(nums),2):
+            result+=min(nums[i],nums[i+1])
+    
+        return result
