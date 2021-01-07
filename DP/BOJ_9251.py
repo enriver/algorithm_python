6322e4db671ca25a0eb4cdfef9aceb8afdb8bdae
enriver <riverkeh@naver.com>
2021-01-07 13:07:04 +0900

    UPDATE BOJ_9251

--- DP/BOJ_9251.py
+++ DP/BOJ_9251.py
@@ -5,7 +5,7 @@ import sys
 first=sys.stdin.readline().strip()
 second=sys.stdin.readline().strip()
 
-dp=[[0 for _ in range(len(first)+1)] for _ in range(len(second)+1)]
+dp=[[0 for _ in range(len(second)+1)] for _ in range(len(first)+1)]
 
 for i in range(1, len(first)+1):
     for j in range(1, len(second)+1):
