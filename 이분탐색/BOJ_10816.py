7eb0c49def041071f2cb8dc61c7ec0e3733870b1
enriver <riverkeh@naver.com>
2021-01-10 23:17:43 +0900

    ADD BOJ_10816

--- 이분탐색/BOJ_10816.py
+++ 이분탐색/BOJ_10816.py
@@ -1,1 +1,23 @@
 # 숫자 카드 2 - S4
+
+import sys
+
+N=int(sys.stdin.readline())
+arr_n=list(map(int,sys.stdin.readline().split()))
+
+M=int(sys.stdin.readline())
+arr_m=list(map(int,sys.stdin.readline().split()))
+
+dic = dict()
+
+for i in arr_n:
+    try :
+        dic[i] += 1
+    except:
+        dic[i] = 1
+
+for i in arr_m:
+    try:
+        print(dic[i] , end = " ")
+    except:
+        print(0, end=" ")
