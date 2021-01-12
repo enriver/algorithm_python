8095b45580ef8d726385025175825f256991d2c2
enriver <riverkeh@naver.com>
2021-01-12 23:57:32 +0900

    UPDATE BOJ_5904

--- DP/BOJ_5904.py
+++ DP/BOJ_5904.py
@@ -2,29 +2,31 @@
 
 import sys
 
+# S_{0}='moo'
+# S_{n+1}=S_{n}+'m'+'o'*(n+2)+S_{n}  (n>=0)
 
-def solve(n,k):
+def solve(n,k): 
     if k==0:
         return 'moo'[n-1]
     
-    if n<=dp[k-1]:
+    if n<=dp[k-1]: # n<= S_{n-1} 인 경우
         return solve(n,k-1)
-    elif n<=dp[k-1]+k+3:
-        if n-dp[k-1]-1==0:
+    elif n<=dp[k-1]+k+3: # n<=S_{n-1}+n+3 인 경우
+        if n-dp[k-1]==1: # 남은 글자수가 1 : m
             return 'm'
-        else:
+        else: # else: o
             return 'o'
-    else:
+    else: # S_{n-1}+n+3< n 인 경우
         return solve(n-dp[k-1]-(k+3),k-1)
 
 if __name__=="__main__":
     N=int(sys.stdin.readline())
 
-    dp=[3]
+    dp=[3] # 문자의 길이를 넣어주는 리스트
     K=0
 
-    while dp[K]<N:
+    while dp[K]<N: # 조건 : S_{k}의 길이가 N보다 작을 때
         K+=1
-        dp.append(2*dp[K-1]+K+3)
+        dp.append(2*dp[K-1]+K+3) #  K가 증가할때마다 생성되는 S_{k}의 길이 추가
 
     print(solve(N,K))
