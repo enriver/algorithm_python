ef666662c6ce26572515172078504b65b9ba8447
enriver <riverkeh@naver.com>
2021-03-23 22:56:20 +0900

    MODIFY BOJ_16197

--- BFS DFS/BOJ_16197.py
+++ BFS DFS/BOJ_16197.py
@@ -14,38 +14,39 @@ def solve(coins):
         coin_2=que.popleft()
         cnt=que.popleft()
 
+        if cnt>10:
+            break
+
         for i in range(4):
             ax,ay=coin_1[0]+dx[i], coin_1[1]+dy[i]
             bx,by=coin_2[0]+dx[i], coin_2[1]+dy[i]
 
             if 0<=ax<N and 0<=ay<M:
-                if not visit[0][ax][ay] and board[ax][ay]!='#':
-                    if 0>bx or N<=bx or 0>by or M<=by:
-                        print(cnt)
-                        return
+                if 0>bx or N<=bx or 0>by or M<=by:
+                    print(cnt)
+                    return
+
             if 0<=bx<N and 0<=by<M:
-                if not visit[1][bx][by] and board[bx][by]!='#':
-                    if 0>ax or N<=ax or 0>ay or M<=ay:
-                        print(cnt)
-                        return
+                if 0>ax or N<=ax or 0>ay or M<=ay:
+                    print(cnt)
+                    return
 
             if 0<=ax<N and 0<=ay<M and 0<=bx<N and 0<=by<M:
-                if board[ax][ay]!='#' and board[bx][by]!='#': # 둘다 #이 아닌 경우 -> 둘중 하나라도 움직일 수 있는 상황
-                    if not visit[0][ax][ay] and not visit[1][bx][by]:
-                        visit[0][ax][ay]=True
-                        visit[1][bx][by]=True
-
-                        que.append((ax,ay))
-                        que.append((bx,by))
-                        que.append(cnt+1)
-
-                if board[ax][ay]!='#' and board[bx][by]=='#':
+                if board[ax][ay]!='#' and board[bx][by]=='#': # 첫번째 동전만 움직이는 경우
+                    
                     que.append((ax,ay))
-                    que.append((coin_2[0],coin_2[1]))
+                    que.append((coin_2[0], coin_2[1]))
                     que.append(cnt+1)
 
-                if board[bx][by]!='#' and board[ax][ay]=='#':
+                if board[ax][ay]=='#' and board[bx][by]!='#': # 두번째 동전만 움직이는 경우
+
                     que.append((coin_1[0], coin_1[1]))
+                    que.append((bx,by))
+                    que.append(cnt+1)
+
+                if board[ax][ay]!='#' and board[bx][by]!='#' : # 두 동전 모두 움직이는 경우
+                
+                    que.append((ax,ay))
                     que.append((bx,by))
                     que.append(cnt+1)
 
@@ -56,7 +57,6 @@ if __name__=="__main__":
     N,M=map(int,sys.stdin.readline().split()) # N세로, M가로
 
     board=list()
-    visit=[[[False for _ in range(M)] for _ in range(N)] for _ in range(2)]
 
     for _ in range(N):
         board.append(list(sys.stdin.readline().rstrip()))
@@ -65,11 +65,9 @@ if __name__=="__main__":
     dx=[1,-1,0,0]
     dy=[0,0,1,-1]
 
-    k=0
     for i in range(N):
         for j in range(M):
             if board[i][j]=='o':
-                coin.append((i,j))
-                visit[k][i][j]=True
-                k+=1
+                coin.append((i,j))                
+                
     solve(coin)
