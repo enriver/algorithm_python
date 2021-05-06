bb710dcc751555f99a706d022563ee4bd86d7e00
enriver <riverkeh@naver.com>
2021-05-06 16:43:28 +0900

    MODIFY var_name

--- String/BOJ_2922.py
+++ String/BOJ_2922.py
@@ -2,31 +2,31 @@
 
 import sys
 
-def dfs(idx, moeum, jaeum, L):
+def dfs(idx, moeum, jaeum, containL):
     cnt=0
 
     if moeum>=3 or jaeum>=3: # 모음이나 자음이 연속해서 3번 나왔는지 체크
         return 0
 
     if idx==len(word): # 모든 인덱스를 돌았을 때 'L'을 포함했는가
-        if L:
+        if containL:
             return 1
         else:
             return 0
 
     if word[idx]=="_":
-        cnt+=5*dfs(idx+1, moeum+1, 0, L) # 모음
-        cnt+=20*dfs(idx+1,0,jaeum+1,L) # 'L'을 제외한 자음
+        cnt+=5*dfs(idx+1, moeum+1, 0, containL) # 모음
+        cnt+=20*dfs(idx+1,0,jaeum+1,containL) # 'L'을 제외한 자음
         cnt+=dfs(idx+1,0,jaeum+1,True) # 'L'을 포함한 자음
 
     elif word[idx] in 'AEIOU':
-        cnt+=dfs(idx+1, moeum+1, 0, L)
+        cnt+=dfs(idx+1, moeum+1, 0, containL)
 
     elif word[idx] not in 'AEIOU':
         if word[idx]=='L':
             cnt+=dfs(idx+1,0,jaeum+1,True)
         else:
-            cnt+=dfs(idx+1,0,jaeum+1,L)
+            cnt+=dfs(idx+1,0,jaeum+1,containL)
 
     return cnt
 
