0a919581cc1edca3b705c9407acc0cdff1852d4d
강의현 <riverkeh@naver.com>
2021-08-05 17:13:20 +0900

    MODIFY BOJ_1932

--- DP/BOJ_1932.py
+++ DP/BOJ_1932.py
@@ -8,19 +8,13 @@ val=[]
 for _ in range(n):
     val.append(list(map(int,sys.stdin.readline().split())))
 
-
-val[1][0]=val[1][0]+val[0][0]
-val[1][1]=val[1][1]+val[0][0]
-
-k=3
-for i in range(2,n):
-    for j in range(k):
-        if j==0:
-            val[i][j]=val[i][j]+val[i-1][j]
-        elif j==(k-1):
-            val[i][-1]=val[i][-1]+val[i-1][-1]
+for i in range(1,n):
+    for j in range(i+1):
+        if j==0: # 맨 왼쪽인 경우
+            val[i][j]+=val[i-1][j] 
+        elif j==i: # 맨 오른쪽인 경우
+            val[i][j]+=val[i-1][j-1]
         else:
-            val[i][j]=max(val[i][j]+val[i-1][j-1],val[i][j]+val[i-1][j])
-    k+=1
+            val[i][j]+=max(val[i-1][j-1], val[i-1][j])
 
 print(max(val[n-1]))
